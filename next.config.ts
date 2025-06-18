ff1ba75378406ad28a8c961180000ede4f325bb0
Ashay Aswale <asaswale@wpi.edu>
2025-06-17 21:35:27 -0400

    publishing attempt to gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/unstackle',
+  assetPrefix: '/unstackle',
+  trailingSlash: true,
+  output: 'export',
+  distDir: 'build'
 };
 
-export default nextConfig;
+module.exports = nextConfig;
